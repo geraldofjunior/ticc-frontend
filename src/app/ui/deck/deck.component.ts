b3e43f8c2942aa1fd3b0400f7ce6c72140a0a37b
Geraldo de Figueiredo Júnior <geraldofjunior@gmail.com>
2024-08-23 10:06:36 -0300

    Code clean-up on deck component

--- src/app/ui/deck/deck.component.ts
+++ src/app/ui/deck/deck.component.ts
@@ -1,6 +1,6 @@
 import { CardComponent } from './../card/card.component';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { AfterViewInit, Component, ComponentRef, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Deck } from 'src/app/entities/deck';
 
 @Component({
@@ -18,13 +18,10 @@ import { Deck } from 'src/app/entities/deck';
   ]
 })
 export class DeckComponent implements OnInit, AfterViewInit {
-  private cards: Element[] = [];
   public cardComponents: ComponentRef<CardComponent>[] = [];
   @ViewChild("board", { read: ViewContainerRef, static: false }) board!: ViewContainerRef;
 
   public deck: Deck = new Deck();
-
-  public constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     this.initializeDeck();
@@ -40,13 +37,12 @@ export class DeckComponent implements OnInit, AfterViewInit {
     const randomizedDeck = deck.shuffle();
     this.deck.setDeck(randomizedDeck);
 
-    const root = document.querySelector("#deck");
-    if (root)
-      root.innerHTML = "";
+    this.board.clear();
+    this.addCardComponents();
+  }
 
-    this.createDeck();
-    this.deck.getCards().forEach(card => console.log(card.getName()));
-    console.log("-----");
+  public animateStack(): void {
+    //// TODO: Implementar animateStack()
   }
 
   public animateShuffle(): void {
@@ -59,51 +55,6 @@ export class DeckComponent implements OnInit, AfterViewInit {
 
   private animateSpread() {
     //// TODO: Implementar animateSpread
-  }
-
-  /** @deprecated
-   * It doesn't support any animation
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public createElement(tagName: string, attributes: any, children: any = null) {
-    const element = this.renderer.createElement(tagName);
-    if (attributes) {
-      for (const attrName in attributes) {
-        element.setAttribute(attrName, attributes[attrName]);
-      }
-    }
-    if (children) {
-      for (let i = 0; i < children.length; i++) {
-        const child = children[i];
-        if (typeof child === 'string') {
-          element.appendChild(document.createTextNode(child));
-        } else {
-          element.appendChild(child);
-        }
-      }
-    }
-    return element;
-  }
-  /** @deprecated
-   * It doesn't support any animation
-   */
-  public createCard(i:number) {
-    const card = this.deck.getCard(i);
-    const filename = "../../../assets/card-images/" + card.getFileName();
-
-    return this.createElement('div', { class: 'card black', style: "background-image: url(" + filename + ")" });
-  }
-  /** @deprecated
-   * It doesn't support any animation
-   */
-  public createDeck() {
-    const deckElement = document.getElementById("deck");
-
-    for (let i = 0; i < this.deck.getCardQuantity(); i++) {
-      const cardElement = this.createCard(i);
-      deckElement?.appendChild(cardElement);
-      this.cards.push(cardElement);
-    }
   }
 
   public addCardComponents() {
